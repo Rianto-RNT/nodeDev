032bb30dfca29fce5fa0abbe53521981f2dc9b35
Rianto-RNT <rianto.rnt@gmail.com>
2021-09-21 16:27:00 +0800

    + Just Breathe

---
+++ finishYourNodeApp/inhale-and-exhale.js
@@ -0,0 +1,20 @@
+console.log("inhale");
+setTimeout(() => {
+  console.log(".");
+}, 1000);
+
+setTimeout(() => {
+  console.log("Exhale");
+}, 2000);
+
+setTimeout(() => {
+  console.log(".");
+}, 3000);
+
+setTimeout(() => {
+  console.log(".");
+}, 4000);
+
+setTimeout(() => {
+  console.log("and", ".");
+}, 5000);
